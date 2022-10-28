1ac1bc29c72e62816e9c7a3ad658b8187c99f676
PradipSapkota <93136486+PradipSapkota@users.noreply.github.com>
2022-10-28 11:18:29 +0545

    Remaining files upload

---
+++ precache-manifest.561a5429984d1c1ef541729911d78aff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2d5b12d472ccbd541416",
+    "url": "/static/js/main.2d5b12d4.chunk.js"
+  },
+  {
+    "revision": "f7863ee15824e544f112",
+    "url": "/static/js/1.f7863ee1.chunk.js"
+  },
+  {
+    "revision": "2d5b12d472ccbd541416",
+    "url": "/static/css/main.fe7a221d.chunk.css"
+  },
+  {
+    "revision": "7c2f1275fb409efdb33e7e1b351d0e63",
+    "url": "/index.html"
+  }
+];
